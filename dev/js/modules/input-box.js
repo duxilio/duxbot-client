97d3e7d6c7149aba82203fb4dd4da82f03c30c75
Koen Vendrik <k.vendrik@gmail.com>
2015-10-29 17:10:26 +0100

    Adds listen hotkey

--- dev/js/modules/input-box.js
+++ dev/js/modules/input-box.js
@@ -25,7 +25,6 @@
 
 		_initSpeech: function(){
 			var speech = this._speech = new DX.speechHelper();
-			this._listening = false;
 
 			var input = this._els.input,
 				startBtn = this._els.startBtn,
@@ -50,15 +49,21 @@
 			});
 
 			//on startbutton click, toggle listening
-			startBtn.addEventListener('click', function(){
-				if(!self._listening){
-					self._els.startBtn.classList.add('input-box__input-wrapper__icon--active');
-					speech.start();
-					self._listening = true;
-				} else {
-					self._stopSpeechListening(true);
-				}
-			}, false);
+			startBtn.addEventListener('click', this._toggleSpeechListening.bind(this), false);
+		},
+
+		_toggleSpeechListening: function(){
+			if(!this._listening){
+				this._startSpeechListening();
+			} else {
+				this._stopSpeechListening(false);
+			}
+		},
+
+		_startSpeechListening: function(){
+			this._els.startBtn.classList.add('input-box__input-wrapper__icon--active');
+			this._speech.start();
+			this._listening = true;
 		},
 
 		_stopSpeechListening: function(insertLastResult){
@@ -82,60 +87,65 @@
 				val = input.value,
 				keyCode = e.keyCode;
 
-			this._stopSpeechListening();
+			if(keyCode === 18){
+				this._toggleSpeechListening();
+				return;
+			}
 
-			//if enter is pressed
-			if(keyCode === 13){ //enter
-				if(!val) return;
-				input.value = '';
+			this._stopSpeechListening(true);
 
-				//DEBUG
-				if(val === 'clear'){
-					DX.outputBox.clear();
-					return;
-				} else if(val.indexOf('card') !== -1){
-					DX.outputBox.add({
-						template: 'card',
-						data: {
-							details: {
-								name: {
-									title: 'Name',
-									content: 'Software Architecture'
-								},
-								date: {
-									title: 'Date',
-									content: '29th of October 2015 (tomorrow)'
-								},
-								time: {
-									title: 'Time',
-									content: '4pm - 5pm'
-								},
-								participants: {
-									title: 'Participants',
-									tags: ['Koen', 'Matti', 'Dave (Microsoft)', 'George (Microsoft)']
+			switch(keyCode){
+				case 13: //enter
+					if(!val) return;
+					input.value = '';
+
+					//DEBUG
+					if(val === 'clear'){
+						DX.outputBox.clear();
+						return;
+					} else if(val.indexOf('card') !== -1){
+						DX.outputBox.add({
+							template: 'card',
+							data: {
+								details: {
+									name: {
+										title: 'Name',
+										content: 'Software Architecture'
+									},
+									date: {
+										title: 'Date',
+										content: '29th of October 2015 (tomorrow)'
+									},
+									time: {
+										title: 'Time',
+										content: '4pm - 5pm'
+									},
+									participants: {
+										title: 'Participants',
+										tags: ['Koen', 'Matti', 'Dave (Microsoft)', 'George (Microsoft)']
+									}
 								}
 							}
-						}
-					});
-				} else {
-					DX.outputBox.add({
-						template: 'message',
-						data: {
-							content: val,
-							right: val === 'hola'
-						}
-					});
-				}
-			} else {
-
-				//if there is a keyup event in doc
-				//and the search input is not focussed
-				//focus the input
-				if(document.activeElement !== input){
-					input.value = String.fromCharCode(keyCode);
-					input.focus();
-				}
-
+						});
+					} else {
+						DX.outputBox.add({
+							template: 'message',
+							data: {
+								content: val,
+								right: val === 'hola'
+							}
+						});
+					}
+					break;
+				default:
+					//if there is a keyup event in doc
+					//and the search input is not focussed
+					//focus the input
+					if(document.activeElement !== input){
+						input.value = String.fromCharCode(keyCode);
+						input.focus();
+					}
+					break;
 			}
 		}
 
--- public/js/app.js
+++ public/js/app.js
@@ -4633,7 +4633,6 @@ return /******/ (function(modules) { // webpackBootstrap
 
 		_initSpeech: function(){
 			var speech = this._speech = new DX.speechHelper();
-			this._listening = false;
 
 			var input = this._els.input,
 				startBtn = this._els.startBtn,
@@ -4658,15 +4657,21 @@ return /******/ (function(modules) { // webpackBootstrap
 			});
 
 			//on startbutton click, toggle listening
-			startBtn.addEventListener('click', function(){
-				if(!self._listening){
-					self._els.startBtn.classList.add('input-box__input-wrapper__icon--active');
-					speech.start();
-					self._listening = true;
-				} else {
-					self._stopSpeechListening(true);
-				}
-			}, false);
+			startBtn.addEventListener('click', this._toggleSpeechListening.bind(this), false);
+		},
+
+		_toggleSpeechListening: function(){
+			if(!this._listening){
+				this._startSpeechListening();
+			} else {
+				this._stopSpeechListening(false);
+			}
+		},
+
+		_startSpeechListening: function(){
+			this._els.startBtn.classList.add('input-box__input-wrapper__icon--active');
+			this._speech.start();
+			this._listening = true;
 		},
 
 		_stopSpeechListening: function(insertLastResult){
@@ -4690,60 +4695,65 @@ return /******/ (function(modules) { // webpackBootstrap
 				val = input.value,
 				keyCode = e.keyCode;
 
-			this._stopSpeechListening();
+			if(keyCode === 18){
+				this._toggleSpeechListening();
+				return;
+			}
 
-			//if enter is pressed
-			if(keyCode === 13){ //enter
-				if(!val) return;
-				input.value = '';
+			this._stopSpeechListening(true);
 
-				//DEBUG
-				if(val === 'clear'){
-					DX.outputBox.clear();
-					return;
-				} else if(val.indexOf('card') !== -1){
-					DX.outputBox.add({
-						template: 'card',
-						data: {
-							details: {
-								name: {
-									title: 'Name',
-									content: 'Software Architecture'
-								},
-								date: {
-									title: 'Date',
-									content: '29th of October 2015 (tomorrow)'
-								},
-								time: {
-									title: 'Time',
-									content: '4pm - 5pm'
-								},
-								participants: {
-									title: 'Participants',
-									tags: ['Koen', 'Matti', 'Dave (Microsoft)', 'George (Microsoft)']
+			switch(keyCode){
+				case 13: //enter
+					if(!val) return;
+					input.value = '';
+
+					//DEBUG
+					if(val === 'clear'){
+						DX.outputBox.clear();
+						return;
+					} else if(val.indexOf('card') !== -1){
+						DX.outputBox.add({
+							template: 'card',
+							data: {
+								details: {
+									name: {
+										title: 'Name',
+										content: 'Software Architecture'
+									},
+									date: {
+										title: 'Date',
+										content: '29th of October 2015 (tomorrow)'
+									},
+									time: {
+										title: 'Time',
+										content: '4pm - 5pm'
+									},
+									participants: {
+										title: 'Participants',
+										tags: ['Koen', 'Matti', 'Dave (Microsoft)', 'George (Microsoft)']
+									}
 								}
 							}
-						}
-					});
-				} else {
-					DX.outputBox.add({
-						template: 'message',
-						data: {
-							content: val,
-							right: val === 'hola'
-						}
-					});
-				}
-			} else {
-
-				//if there is a keyup event in doc
-				//and the search input is not focussed
-				//focus the input
-				if(document.activeElement !== input){
-					input.value = String.fromCharCode(keyCode);
-					input.focus();
-				}
-
+						});
+					} else {
+						DX.outputBox.add({
+							template: 'message',
+							data: {
+								content: val,
+								right: val === 'hola'
+							}
+						});
+					}
+					break;
+				default:
+					//if there is a keyup event in doc
+					//and the search input is not focussed
+					//focus the input
+					if(document.activeElement !== input){
+						input.value = String.fromCharCode(keyCode);
+						input.focus();
+					}
+					break;
 			}
 		}
 
